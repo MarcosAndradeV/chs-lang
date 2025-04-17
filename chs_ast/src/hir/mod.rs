378e8ae5813cd2d076bef2d774f9820bb59acdf9
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-04-17 00:05:49 -0300

    Add HIR module

---
+++ chs_ast/src/hir/mod.rs
@@ -0,0 +1,247 @@
+use chs_lexer::Span;
+use chs_types::CHSType;
+
+use crate::{nodes, RawModule};
+
+use crate::nodes::Operator as HIROperator;
+
+#[derive(Debug)]
+pub struct HIRModule<'src> {
+    pub raw_module: &'src RawModule,
+    pub items: Vec<HIRModuleItem>,
+}
+
+impl<'src> HIRModule<'src> {
+    pub fn from_ast_module(ast: nodes::Module<'src>) -> Self {
+        Self {
+            raw_module: ast.raw_module,
+            items: ast
+                .items
+                .into_iter()
+                .map(|item| match item {
+                    nodes::ModuleItem::Function(func) => {
+                        HIRModuleItem::Function(HIRFunction::from_ast_function(func))
+                    }
+                    nodes::ModuleItem::ExternFunction(func) => HIRModuleItem::ExternFunction(
+                        HIRExternFunction::from_ast_extern_function(func),
+                    ),
+                })
+                .collect(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum HIRModuleItem {
+    Function(HIRFunction),
+    ExternFunction(HIRExternFunction),
+}
+
+#[derive(Debug)]
+pub struct HIRFunction {
+    pub name: Span<String>,
+    pub fn_type: CHSType,
+    pub params: Vec<HIRParam>,
+    pub return_type: CHSType,
+    pub body: Vec<HIRExpr>,
+}
+impl HIRFunction {
+    fn from_ast_function(func: nodes::FunctionDecl) -> HIRFunction {
+        Self {
+            name: func.name.source,
+            fn_type: func.fn_type,
+            params: func
+                .params
+                .into_iter()
+                .map(HIRParam::from_ast_param)
+                .collect(),
+            return_type: func.ret_type,
+            body: func.body.into_iter().map(HIRExpr::from_ast_expr).collect(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct HIRExternFunction {
+    pub name: Span<String>,
+    pub fn_type: CHSType,
+}
+impl HIRExternFunction {
+    fn from_ast_extern_function(func: nodes::ExternFunctionDecl) -> HIRExternFunction {
+        Self {
+            name: func.name.source,
+            fn_type: func.fn_type,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct HIRParam {
+    pub name: Span<String>,
+    pub param_type: CHSType,
+}
+
+impl HIRParam {
+    fn from_ast_param(param: nodes::Param) -> HIRParam {
+        Self {
+            name: param.name.source,
+            param_type: param.ty,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct HIRBlock {
+    pub expressions: Vec<HIRExpr>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ExprId(pub usize);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct VarId(pub usize);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ScopeId(pub usize);
+
+// Maybe use this
+// #[derive(Debug)]
+// pub struct WithSpan<T> {
+//     pub node: T,
+//     pub span: Span<String>,
+// }
+
+#[derive(Debug)]
+pub enum HIRExpr {
+    Literal(HIRLiteral),
+    Identifier(Span<String>),
+    Binary {
+        op: HIROperator,
+        lhs: Box<HIRExpr>,
+        rhs: Box<HIRExpr>,
+    },
+    Unary {
+        op: HIROperator,
+        operand: Box<HIRExpr>,
+    },
+    Call {
+        callee: Box<HIRExpr>,
+        args: Vec<HIRExpr>,
+    },
+    Cast {
+        expr: Box<HIRExpr>,
+        to_type: CHSType,
+    },
+    Index {
+        base: Box<HIRExpr>,
+        index: Box<HIRExpr>,
+    },
+    Assign {
+        target: Box<HIRExpr>,
+        value: Box<HIRExpr>,
+    },
+    VarDecl {
+        name: Span<String>,
+        ty: Option<CHSType>,
+        value: Box<HIRExpr>,
+    },
+    Block(HIRBlock),
+    If {
+        condition: Box<HIRExpr>,
+        then_branch: HIRBlock,
+        else_branch: Option<HIRBlock>,
+    },
+    While {
+        condition: Box<HIRExpr>,
+        body: HIRBlock,
+    },
+    Syscall {
+        arity: usize,
+        args: Vec<HIRExpr>,
+    },
+    Return(Option<Box<HIRExpr>>)
+}
+
+impl HIRExpr {
+    fn from_ast_expr(expr: nodes::Expression) -> Self {
+        match expr {
+            nodes::Expression::ConstExpression(cexp) => match cexp {
+                nodes::ConstExpression::IntegerLiteral(s) => HIRExpr::Literal(HIRLiteral::Int(s)),
+                nodes::ConstExpression::BooleanLiteral(s) => HIRExpr::Literal(HIRLiteral::Bool(s)),
+                nodes::ConstExpression::Identifier(s) => HIRExpr::Identifier(s),
+                nodes::ConstExpression::StringLiteral(s) => HIRExpr::Literal(HIRLiteral::Str(s)),
+                nodes::ConstExpression::CharLiteral(s) => HIRExpr::Literal(HIRLiteral::Char(s)),
+            },
+            nodes::Expression::Binop(b) => HIRExpr::Binary {
+                op: b.op,
+                lhs: HIRExpr::from_ast_expr(b.left).into(),
+                rhs: HIRExpr::from_ast_expr(b.right).into(),
+            },
+            nodes::Expression::Unop(u) => HIRExpr::Unary {
+                op: u.op,
+                operand: HIRExpr::from_ast_expr(u.operand).into(),
+            },
+            nodes::Expression::Call(c) => HIRExpr::Call {
+                callee: HIRExpr::from_ast_expr(c.callee).into(),
+                args: c.args.into_iter().map(HIRExpr::from_ast_expr).collect(),
+            },
+            nodes::Expression::Cast(c) => HIRExpr::Cast {
+                expr: HIRExpr::from_ast_expr(c.casted).into(),
+                to_type: c.to_type,
+            },
+            nodes::Expression::Index(i) => HIRExpr::Index {
+                base: HIRExpr::from_ast_expr(i.base).into(),
+                index: HIRExpr::from_ast_expr(i.index).into(),
+            },
+            nodes::Expression::Syscall(s) => HIRExpr::Syscall {
+                arity: s.arity,
+                args: s.args.into_iter().map(HIRExpr::from_ast_expr).collect(),
+            },
+            nodes::Expression::VarDecl(v) => HIRExpr::VarDecl {
+                name: v.token.source,
+                ty: v.ty,
+                value: HIRExpr::from_ast_expr(v.value).into(),
+            },
+            nodes::Expression::Assign(a) => HIRExpr::Assign {
+                target: HIRExpr::from_ast_expr(a.target).into(),
+                value: HIRExpr::from_ast_expr(a.value).into(),
+            },
+            nodes::Expression::Group(e) => HIRExpr::from_ast_expr(*e),
+            nodes::Expression::IfExpression(i) => HIRExpr::If {
+                condition: HIRExpr::from_ast_expr(i.cond).into(),
+                then_branch: HIRBlock {
+                    expressions: i.body.into_iter().map(HIRExpr::from_ast_expr).collect(),
+                },
+                else_branch: None,
+            },
+            nodes::Expression::IfElseExpression(i) => HIRExpr::If {
+                condition: HIRExpr::from_ast_expr(i.cond).into(),
+                then_branch: HIRBlock {
+                    expressions: i.body.into_iter().map(HIRExpr::from_ast_expr).collect(),
+                },
+                else_branch: Some(HIRBlock {
+                    expressions: i
+                        .else_body
+                        .into_iter()
+                        .map(HIRExpr::from_ast_expr)
+                        .collect(),
+                }),
+            },
+            nodes::Expression::WhileExpression(w) => HIRExpr::While {
+                condition: HIRExpr::from_ast_expr(w.cond).into(),
+                body: HIRBlock {
+                    expressions: w.body.into_iter().map(HIRExpr::from_ast_expr).collect(),
+                },
+            },
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum HIRLiteral {
+    Int(Span<i64>),
+    Bool(Span<bool>),
+    Str(Span<String>),
+    Char(Span<char>),
+    Void,
+}
