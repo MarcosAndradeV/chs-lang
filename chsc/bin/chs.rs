6ed1408e6959683b5f23ce5416df098a32552891
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-04-17 22:40:43 -0300

    Stub out compilation logic and remove dead code

--- chsc/bin/chs.rs
+++ chsc/bin/chs.rs
@@ -12,7 +12,6 @@ use std::{
 use chs_ast::{
     hir::HIRModule, mir::MIRModule, parser::Parser, typechecker::TypeChecker, RawModule,
 };
-use chs_codegen::mir::MIRTranslator;
 use chs_util::{return_chs_error, CHSError, CHSResult};
 
 use my_cli::{Cmd, MyCLI};
@@ -26,6 +25,7 @@ fn main() {
                 .help("Compiles the program.")
                 .arg("INPUT", 0)
                 .flag("o", "OUTPUT", false)
+                .flag("l", "LIB", false)
                 .flag_bool("r")
                 .flag_bool("s")
                 .flag_bool("emit-asm"),
@@ -71,82 +71,7 @@ fn compile(
     let tenv = checker.env();
     let module = MIRModule::from_hir(module, tenv);
 
-    let fasm_code = MIRTranslator::new(&raw_module).translate_module(module);
-
-    let fasm_path = PathBuf::from(&raw_module.file_path).with_extension("asm");
-    let mut out_file = File::create(&fasm_path).map_err(|err| CHSError(err.to_string()))?;
-    write!(out_file, "{}", fasm_code).map_err(|err| CHSError(err.to_string()))?;
-    if !silent {
-        println!("[INFO] Generating {}", fasm_path.display());
-    }
-    let fasm_output_path = match outpath {
-        Some(ref a) => PathBuf::from(a).with_extension("o"),
-        None => fasm_path.with_extension("o"),
-    };
-    let mut fasm_proc = process::Command::new("fasm");
-    fasm_proc.arg(&fasm_path).arg(&fasm_output_path);
-
-    if silent {
-        fasm_proc.stdout(Stdio::null());
-    }
-
-    let result = fasm_proc
-        .spawn()
-        .expect("Failed to spawn fasm process")
-        .wait_with_output()
-        .expect("Failed to wait for fasm");
-    if !result.status.success() {
-        return_chs_error!(String::from_utf8_lossy(&result.stderr));
-    }
-
-    let gcc_output_path = match outpath {
-        Some(a) => PathBuf::from(a),
-        None => fasm_path.with_extension(""),
-    };
-
-    let mut gcc_proc = process::Command::new("gcc");
-    gcc_proc
-        .arg("-static")
-        .arg("-o")
-        .arg(&gcc_output_path)
-        .arg(&fasm_output_path);
-
-    if silent {
-        gcc_proc.stdout(Stdio::null());
-    }
-
-    let result = gcc_proc
-        .spawn()
-        .expect("Failed to spawn gcc process")
-        .wait_with_output()
-        .expect("Failed to wait for gcc");
-    if !result.status.success() {
-        return_chs_error!(String::from_utf8_lossy(&result.stderr));
-    }
-
-    if !emit_asm {
-        let mut rm_proc = process::Command::new("rm")
-            .arg(fasm_path)
-            .arg(fasm_output_path)
-            .spawn()
-            .expect("Failed to spawn rm process");
-
-        let result = rm_proc.wait_with_output().expect("Failed to wait for rm");
-        if !result.status.success() {
-            return_chs_error!("Failed {}", String::from_utf8_lossy(&result.stderr));
-        }
-    }
-
-    if run {
-        let mut run_proc = process::Command::new(format!("./{}", gcc_output_path.display()))
-            .spawn()
-            .expect("Failed to spawn run process");
-
-        let result = run_proc.wait_with_output().expect("Failed to wait for run");
-        if !result.status.success() {
-            return_chs_error!("Failed {}", String::from_utf8_lossy(&result.stderr));
-        }
-    }
+    todo!("Finalize compilation");
 
     Ok(())
 }
