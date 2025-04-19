799d95cd56001ed2eac2d1618d7fa78f98537994
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-04-19 14:22:54 -0300

    Replace cli tool

---
+++ chsc/src/cli.rs
@@ -0,0 +1,37 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "chsc")]
+#[command(author = "Marcos V. Andrade <>")]
+#[command(version = "0.1")]
+#[command(about = "Compile and run chs files", long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Compile a source file
+    Compile {
+        /// Source file to compile
+        input: String,
+
+        /// Output file name
+        #[arg(short, long)]
+        output: Option<String>,
+
+        /// Silences compiler output
+        #[arg(short, long)]
+        silent: bool,
+    },
+
+    /// Compile and run the executable
+    CompileRun {
+        input: String,
+
+        #[arg(short, long)]
+        output: Option<String>,
+
+    },
+}
