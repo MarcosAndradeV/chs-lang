5278cf91934fb286744a53d804fd1ec539682adb
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-04-19 14:23:06 -0300

    Create config.rs

---
+++ chsc/src/config.rs
@@ -0,0 +1,16 @@
+
+#[derive(Debug)]
+/// Configuration for the chsc tool.
+pub struct Config {}
+
+impl Config {
+    pub fn new() -> Self {
+        Config {}
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {}
+    }
+}
